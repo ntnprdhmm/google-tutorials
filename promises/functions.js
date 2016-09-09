f186c9da2d5415fcfac72c90d2d153fef0edc926
prudywsh <antoineprudhomme5@gmail.com>
2016-09-09 21:59:37 +0200

    starting promises tutorial

---
+++ promises/functions.js
@@ -0,0 +1,30 @@
+/**
+ * Make a GET request
+ */
+function get(url) {
+    return new Promise(function(resolve, reject) {
+        var req = new XMLHttpRequest();
+        req.open('GET', url);
+        // if response
+        req.onload = function() {
+            if(req.status == 200) {
+                resolve(req.response);
+            } else {
+                reject(Error(req.statusText));
+            }
+        };
+        // network error
+        req.onerror = function(){
+            reject(Error('Network Error.'));
+        };
+        // send the request
+        req.send();
+    });
+}
+
+/**
+ * Call the get method, but parse the JSON response
+ */
+function getJSON(url) {
+    return get(url).then(JSON.parse);
+}
