d31517d58ab2371bc40cd2b18c58272b516f8acb
prudywsh <antoineprudhomme5@gmail.com>
2016-09-23 23:32:29 +0200

    remove cards ok

--- swipeable-cards/cards.js
+++ swipeable-cards/cards.js
@@ -9,9 +9,11 @@ class Cards {
         this.onEnd = this.onEnd.bind(this);
         this.update = this.update.bind(this);
 
+        this.targetBCR = null;  // bounding client rect (element size and position)
         this.target = null;
         this.draggingCard = false;
 
+        this.targetX = 0;
         this.startX = 0;
         this.currentX = 0;
         this.screenX = 0;
@@ -33,10 +35,14 @@ class Cards {
     }
 
     onStart (evt) {
+        if (this.target)
+            return;
+
         if (!evt.target.classList.contains('card'))
             return;
 
         this.target = evt.target;
+        this.targetBCR = this.target.getBoundingClientRect();
         this.startX = evt.pageX || evt.touches[0].pageX;
         this.currentX = this.startX;
 
@@ -57,6 +63,12 @@ class Cards {
         if (!this.target)
             return;
 
+        this.targetX = 0; // go back to the middle
+        let screenX = this.currentX - this.startX;
+        if (Math.abs(screenX) > this.targetBCR.width * 0.25) {    // if 25% percent of the width go out the border
+            this.targetX = (screenX > 0) ? this.targetBCR.width : -this.targetBCR.width;
+        }
+
         this.draggingCard = false;
     }
 
@@ -70,11 +82,21 @@ class Cards {
         if (this.draggingCard) {
             this.screenX = this.currentX - this.startX;
         } else {
-            // go to start position (center)
-            this.screenX += (0 - this.screenX) / 10;
+            this.screenX += (this.targetX - this.screenX) / 4;  // more than 4 will be slower (out animation)
         }
 
+        const normalizedDragDistance = (Math.abs(this.screenX) / this.targetBCR.width);
+        const opacity = 1 - Math.pow(normalizedDragDistance, 3); // more the card is far from the middle, less it is visible
+
         this.target.style.transform = `translateX(${this.screenX}px)`;
+        this.target.style.opacity = opacity;
+
+        // remove the card
+        const isNearlyInvisible = (opacity < 0.01);
+        if(!this.draggingCard && isNearlyInvisible) {
+            this.target.parentNode.removeChild(this.target);
+            this.target = null;
+        }
     }
 }
 
