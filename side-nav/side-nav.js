218b31dcb6da12bcf714d7fd65b5781064d18474
prudywsh <antoineprudhomme5@gmail.com>
2016-10-24 21:29:18 +0200

    side nav slide on click

--- side-nav/side-nav.js
+++ side-nav/side-nav.js
@@ -3,6 +3,20 @@
 class SideNav {
     constructor () {
         console.log('SideNav');
+        this.toggleButtonEl = document.querySelector('.js-menu');
+        this.sideNavEl = document.querySelector('.js-side-nav');
+
+        this.showSideNav = this.showSideNav.bind(this);
+
+        this.addEventListeners();
+    }
+
+    addEventListeners () {
+        this.toggleButtonEl.addEventListener('click', this.showSideNav);
+    }
+
+    showSideNav () {
+        this.sideNavEl.classList.add('side-nav--visible');
     }
 }
 
