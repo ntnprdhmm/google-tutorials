61c3435dc239b0c88d1ffb9de65ee739ba3fcfea
prudywsh <antoineprudhomme5@gmail.com>
2016-10-24 23:28:23 +0200

    touch events on the sidebar

--- side-nav/side-nav.js
+++ side-nav/side-nav.js
@@ -6,9 +6,18 @@ class SideNav {
         this.showButtonEl = document.querySelector('.js-menu-show');
         this.hideButtonEl = document.querySelector('.js-menu-hide');
         this.sideNavEl = document.querySelector('.js-side-nav');
+        this.sideNavContainerEl = document.querySelector('.js-side-nav-container');
 
         this.showSideNav = this.showSideNav.bind(this);
         this.hideSideNav = this.hideSideNav.bind(this);
+        this.blockClicks = this.blockClicks.bind(this);
+        this.onTouchStart = this.onTouchStart.bind(this);
+        this.onTouchMove = this.onTouchMove.bind(this);
+        this.onTouchEnd = this.onTouchEnd.bind(this);
+        this.onTransitionEnd = this.onTransitionEnd(this);
+
+        this.startX = 0;
+        this.currentX = 0;
 
         this.addEventListeners();
     }
@@ -16,14 +25,59 @@ class SideNav {
     addEventListeners () {
         this.showButtonEl.addEventListener('click', this.showSideNav);
         this.hideButtonEl.addEventListener('click', this.hideSideNav);
+        this.sideNavEl.addEventListener('click', this.hideSideNav);
+        this.sideNavContainerEl.addEventListener('click', this.blockClicks);
+
+        document.addEventListener('touchstart', this.onTouchStart);
+        document.addEventListener('touchmove', this.onTouchMove);
+        document.addEventListener('touchend', this.onTouchEnd);
+    }
+
+    onTouchStart (evt) {
+        // if we simply click on a link, we don't wan't to move the sidenav
+        if (!this.sideNavEl.classList.contains('side-nav--visible'))
+            return;
+        evt.preventDefault();
+        this.startX = evt.touches[0].pageX;
+        this.currentX = this.startX;
+    }
+
+    onTouchMove (evt) {
+        this.currentX = evt.touches[0].pageX;
+        const translateX = Math.min(0, this.currentX - this.startX);
+        if (translateX < 0) {
+            evt.preventDefault();
+        }
+        this.sideNavContainerEl.style.transform = `translateX(${translateX}px)`;
+    }
+
+    onTouchEnd (evt) {
+        const translateX = Math.min(0, this.currentX - this.startX);
+        if (translateX < 0) {
+            this.sideNavContainerEl.style.transform = '';
+            this.hideSideNav();
+        }
+    }
+
+    blockClicks (evt) {
+        evt.stopPropagation();
+    }
+
+    onTransitionEnd (evt) {
+        this.sideNavEl.classList.remove('side-nav--animatable');
+        this.sideNavEl.removeEventListener('transitionend', this.onTransitionEnd);
     }
 
     showSideNav () {
+        this.sideNavEl.classList.add('side-nav--animatable');
         this.sideNavEl.classList.add('side-nav--visible');
+        this.sideNavEl.addEventListener('transitionend', this.onTransitionEnd);
     }
 
     hideSideNav () {
         this.sideNavEl.classList.remove('side-nav--visible');
+        this.sideNavEl.classList.add('side-nav--animatable');
+        this.sideNavEl.addEventListener('transitionend', this.onTransitionEnd);
     }
 }
 
